1d25f21c02f3cc779ab0bff639cdef3adebb03fa
hikaro <olegzilinskij4@gmail.com>
2025-11-04 01:25:42 +0300

    add some fix lab6

--- lab6/include/MyString.h
+++ lab6/include/MyString.h
@@ -11,15 +11,43 @@ class MyString {
     static const int MAX_LENGTH = 100;
 
 public:
-    MyString(const char* str);
-    ~MyString();
+    explicit MyString(const char* str);
 
     size_t size() const;
     void print() const;
 
-    friend MyString operator+(const MyString& lhs, const MyString& rhs);
-    friend std::ostream& operator<<(std::ostream& os, const MyString& str);
-    friend std::istream& operator>>(std::istream& is, MyString& str);
+    friend MyString operator+(const MyString& lhs, const MyString& rhs) {
+        int newLength = lhs.length + rhs.length;
+        if (newLength > MyString::MAX_LENGTH)
+            throw TooLongString("Ошибка: результат конкатенации слишком длинный");
+        char* newData = new char[newLength + 1];
+        std::strcpy(newData, lhs.data);
+        std::strcat(newData, rhs.data);
+        MyString result(newData);
+        delete[] newData;
+        return result;
+    }
+    
+    friend std::ostream& operator<<(std::ostream& os, const MyString& str) {
+        os << str.data;
+        return os;
+    }
+
+    friend std::istream& operator>>(std::istream& is, MyString& str) {
+        std::string buffer;
+        std::getline(is, buffer);
+
+        if (buffer.length() > MyString::MAX_LENGTH) {
+            throw TooLongString("Ошибка: введенная строка слишком длинная");
+        }
+
+        delete[] str.data;
+        str.length = buffer.length();
+        str.data = new char[str.length + 1];
+        std::strcpy(str.data, buffer.c_str());
+
+        return is;
+    }
 
     static MyString inputMsg(const std::string& prompt = "");
 };
--- lab6/src/MyString.cpp
+++ lab6/src/MyString.cpp
@@ -8,50 +8,12 @@ MyString::MyString(const char* str) : length(std::strlen(str)) {
     std::strcpy(data, str);
 }
 
-
-MyString::~MyString() {
-    delete[] data;
-}
-
 size_t MyString::size() const {
     return length;
 }
 
 void MyString::print() const {
     std::cout << data << std::endl;
-}
-
-MyString operator+(const MyString& lhs, const MyString& rhs) {
-    int newLength = lhs.length + rhs.length;
-    if (newLength > MyString::MAX_LENGTH)
-        throw TooLongString("Ошибка: результат конкатенации слишком длинный");
-    auto newData = new char[newLength + 1];
-    std::strcpy(newData, lhs.data);
-    std::strcat(newData, rhs.data);
-    MyString result(newData);
-    delete[] newData;
-    return result;
-}
-
-std::ostream& operator<<(std::ostream& os, const MyString& str) {
-    os << str.data;
-    return os;
-}
-
-std::istream& operator>>(std::istream& is, MyString& str) {
-    std::string buffer;
-    std::getline(is, buffer);
-
-    if (buffer.length() > MyString::MAX_LENGTH) {
-        throw TooLongString("Ошибка: введенная строка слишком длинная");
-    }
-
-    delete[] str.data;
-    str.length = buffer.length();
-    str.data = new char[str.length + 1];
-    std::strcpy(str.data, buffer.c_str());
-
-    return is;
 }
 
 MyString MyString::inputMsg(const std::string& prompt) {
